4459a8494c35bbe46eab286d170bf5c907121bdc
QuinnVarnell <qavarnell@willamette.edu>
2024-10-14 20:24:26 -0700

    Update Prob3.py

--- Prob3.py
+++ Prob3.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimate time spent (hrs):
+# Name: Quinn Varnell
+# Collaborators: Abygale Brien
+# Estimate time spent (hrs): 1
 ########################################
 
 from pgl import GWindow, GRect, GLabel, GLine
@@ -18,15 +18,34 @@ def clicky_box():
 
     # Defining the callback function, which you won't need until Part C
     def on_mouse_down(event):
-        print("You clicked the window!") # Delete this once you start Part C
-
+        x = event.get_x()
+        y = event.get_y()
+        if gw.get_element_at(x, y) != None: #if there is something at the x and y coordinate, do if statement.
+            n_x = random.randint(0, GW_WIDTH - SQUARE_SIZE)
+            n_y = random.randint(0, GW_HEIGHT - SQUARE_SIZE)
+            rect.set_location(n_x, n_y)
+            points = int(score.get_label())
+            points += 1
+            score.set_label(str(points))
+        else:
+            points = 0
+            score.set_label(str(points))
 
     # Down here you should initialize the window and draw the initial square
     # Make sure you tab it in so that it is part of the clicky_box function
 
     gw = GWindow(GW_WIDTH, GW_HEIGHT)
 
+    rect = GRect(GW_WIDTH/2 - SQUARE_SIZE/2, GW_HEIGHT/2 - SQUARE_SIZE/2, SQUARE_SIZE, SQUARE_SIZE)
+    gw.add(rect)
+    rect.set_filled(True)
+    rect.set_color('dark_green')
 
+    gw.add_event_listener("mousedown", on_mouse_down)
+
+    score = GLabel('0', SCORE_DX, GW_HEIGHT - 10)
+    gw.add(score)
+    score.set_font(SCORE_FONT)
 
 
 
